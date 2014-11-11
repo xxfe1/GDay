b0bcecddc26b6c7ce63919712eeb124b7c60d6a1
lrxxfei <ls.xxfei@gmail.com>
2014-11-11 23:28:44 +1100

    Initial

---
+++ platforms/ios/www/js/controllers.js
@@ -0,0 +1,18 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {
+})
+
+.controller('ServiceCtrl', function($scope) {
+})
+
+.controller('FriendsCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
+
+.controller('AccountCtrl', function($scope) {
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,13 @@
+angular.module('starter.controllers', [])
+
+.controller('SquareCtrl', function($scope) {
+})
+
+.controller('ServiceCtrl', function($scope) {
+})
+
+.controller('MessageCtrl', function($scope) {
+    })
+
+.controller('SettingsCtrl', function($scope) {
+});
